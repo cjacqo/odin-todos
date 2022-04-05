c51b20d79134c134042ca493b3ae81393a914215
cjacqo <contact.jayden.cipriano@gmail.com>
2022-04-05 02:13:20 -0700

    TimeSelector is pretty much complete. Minor bugs with relating the toggle status of the TimePicker to the toggle status of the DatePicker.

--- src/components/forms/inputs/TimeSelector.js
+++ src/components/forms/inputs/TimeSelector.js
@@ -29,112 +29,10 @@ const TimeSelector = (function() {
     let _minutesContainer
     let _ampmContainer
 
-    function _createOrder() {
-        const { hours, minutes, ampm } = _currentTimeObj
-        const { _hourElements, _minuteElements, _ampmElements } = _spinnerElements
-        let minutePositionSet = false
-        _hourElements.filter(el => {
-            let position = el.dataset.hour - hours
-            if (position === 0) {
-                let offset = _hourElements.indexOf(el)
-                for (let i = 0; i < _hourElements.length; i++) {
-                    let pointer = (i + offset) % _hourElements.length
-                    _hourElements[pointer].classList.add(`position-${i}`)
-                }
-            }
-        })
-        _minuteElements.filter(el => {
-            let offset
-            if (el.dataset.minute == minutes) {
-                offset = _minuteElements.indexOf(el)
-                minutePositionSet = true
-                for (let i = 0; i < _minuteElements.length; i++) {
-                    let pointer = (i + offset) % _minuteElements.length
-                    _minuteElements[pointer].classList.add(`position-${i}`)
-                }
-            } else if (el.dataset.minute >= minutes - 5 && el.dataset.minute < minutes) {
-                offset = _minuteElements.indexOf(el)
-                for (let i = 0; i < _minuteElements.length; i++) {
-                    let pointer = (i + offset) % _minuteElements.length
-                    _minuteElements[pointer].classList.add(`position-${i}`)
-                }
-            }
-        })
-        _ampmElements.filter(el => {
-            console.log(el)
-            if (_currentTimeObj.ampm == el.dataset.ampm) {
-                el.classList.add(`ampm-position-1`)
-            } else {
-                el.classList.add(`ampm-position-2`)
-            }
-        })
-    }
-
-    function _createSpinnerElements(type) {
-        let hourElements = []
-        let minuteElements = []
-        let ampmElements = []
-        switch (type) {
-            case 'hours':
-                for (let i = 0; i < 12; i++) {
-                    const element = document.createElement('div')
-                    element.classList.add('spinner-element')
-                    let hour = i
-                    hour = hour % 12
-                    hour = hour ? hour : 12
-                    // let position = _getPosition(hour, _currentTimeObj.hours, 'hours')
-                    element.setAttribute(`data-hour`, hour)
-                    // element.setAttribute('data-position', position)
-                    element.innerText = hour
-                    hourElements.push(element)
-                    _spinnerElements._hourElements.push(element)
-                    // _hoursContainer.appendChild(element)
-                }
-                // _clockContainer.appendChild(_hoursContainer)
-                // _createOrder(hourElements)
-                break
-            case 'minutes':
-                let indexCount = 1
-                for (let i = 0; i < 60; i++) {
-                    if (i % 5 === 0) {
-                        const element = document.createElement('div')
-                        element.classList.add('spinner-element')
-                        let minute = i
-                        element.setAttribute(`data-minute`, minute)
-                        element.setAttribute(`data-index`, indexCount)
-                        element.innerText = minute < 10 ? '0'+minute : minute
-                        minuteElements.push(element)
-                        _spinnerElements._minuteElements.push(element)
-                        indexCount++
-                        // _minutesContainer.appendChild(element)
-                        // _createOrder(minuteElements)
-                    }
-                }
-                // _clockContainer.appendChild(_minutesContainer)
-                break
-            case 'ampm':
-                const amElement = document.createElement('div')
-                const pmElement = document.createElement('div')
-                amElement.classList.add('ampm-element')
-                pmElement.classList.add('ampm-element')
-                amElement.setAttribute('data-ampm', 'am')
-                pmElement.setAttribute('data-ampm', 'pm')
-                amElement.innerText = 'AM'
-                pmElement.innerText = 'PM'
-                ampmElements.push(amElement)
-                ampmElements.push(pmElement)
-                // _createOrder(ampmElements)
-                _spinnerElements._ampmElements.push(amElement)
-                _spinnerElements._ampmElements.push(pmElement)
-                break
-        }
-    }
-
     function _roundToNearestHour() {
         const tempDate = _today
-        tempDate.setHours(tempDate.getHours() + 1)
+        tempDate.setHours(tempDate.getHours() + Math.round(tempDate.getMinutes() / 30))
         tempDate.setMinutes(0, 0, 0)
-        console.log(tempDate.getHours())
         return tempDate
     }
 
@@ -145,6 +43,8 @@ const TimeSelector = (function() {
         } else if (!_isHidden && !_questionAnswer.classList.contains('active')) {
             _questionAnswer.classList.add('active')
             _questionAnswer.innerText = _answer.timeString
+        } else if (!_isHidden) {
+            _questionAnswer.innerText = _answer.timeString
         }
     }
 
@@ -152,35 +52,60 @@ const TimeSelector = (function() {
         if (_isHidden) {
             _timeSelectorInput.classList.add('hidden')
             _hiddenInput.removeAttribute('value')
+            _answer = {}
+            _currentTimeObj = {}
             if (_hiddenInput.value && !isChecked) {
                 // RESET THE INPUT TO DEFAULT
-                console.log(_hiddenInput)
+                _hiddenInput.removeAttribute('value')
             }
+            _updateQuestionAnswerDisplay()
         } else {
             _timeSelectorInput.classList.remove('hidden')
             // DISPLAY THE CLOCK
             if (!_hiddenInput.value && isChecked) {
-                // CREATE THE DEFAULT ANSWER (CLOSEST TIME WITHIN 5 MINUTES) 
-                // WHEN THE INPUT IS TOGGLED ON
-                _answer = _currentTimeObj
+                // CREATE THE DEFAULT ANSWER WHEN THE INPUT IS TOGGLED ON
+                _handleAnswer()
                 _hiddenInput.setAttribute('value', _answer.timeValue)
             }
         }
-        _updateQuestionAnswerDisplay()
     }
 
     function _moduloFix(hours) {
         return ((hours % 12) + 12) % 12;
     }
 
-    function _formatHours(hours) {
+    function _formatHours(hours, needZero) {
         hours = hours % 12
         hours = hours ? hours : 12
+        if (needZero) {
+            return hours >= 10 ? hours : '0' + hours
+        }
         return hours
     }
 
     function _formatMinutes(minutes) {
         return minutes >= 10 ? minutes : '0' + minutes
+    }
+
+    function _timeToString(hours, minutes, ampm) {
+        return hours + ':' + _formatMinutes(minutes) + ' ' + ampm
+    }
+
+    function _timeToValue(hours, minutes, ampm) {
+        const answerAsDate = new Date()
+        answerAsDate.setHours(+hours)
+        answerAsDate.setMinutes(minutes)
+        return hours + ':' + minutes + ' ' + ampm
+    }
+
+    function _formatAnswerObj(hours, minutes, ampm) {
+        _answer = {
+            hours,
+            minutes,
+            ampm,
+            timeString: _timeToString(hours, minutes, ampm),
+            timeValue: _timeToValue(_formatHours(hours, true), _formatMinutes(minutes), ampm)
+        }
     }
 
     function _isAmPm(hours) {
@@ -196,7 +121,7 @@ const TimeSelector = (function() {
         minutes = _formatMinutes(minutes)
         let timeString = hours + ':' + minutes + ' ' + ampm
         let roundedTime = _roundToNearestHour()
-        let roundedHour = _formatHours(roundedTime.getHours() % 12)
+        let roundedHour = _formatHours(roundedTime.getHours())
 
         return _currentTimeObj = {
             timeValue,
@@ -208,31 +133,87 @@ const TimeSelector = (function() {
         }
     }
 
-    function _handleScrollDown(container) {
+    function _handleScrollUp(container) {
         let top = container.children[0].cloneNode(true)
         container.children[0].remove()
         container.appendChild(top)
         _handleStyles(container)
     }
 
-    function _handleScrollUp(container) {
+    function _handleScrollDown(container) {
         let bottom = container.children[container.children.length - 1].cloneNode(true)
         container.children[container.children.length - 1].remove()
         container.prepend(bottom)
         _handleStyles(container)
     }
 
+    function _handleAmPmScrollUp(container) {
+        container.removeAttribute('class')
+        container.classList.add('carousel')
+        container.classList.add('pm-active')
+        _handleStyles(container)
+    }
+
+    function _handleAmPmScrollDown(container) {
+        container.removeAttribute('class')
+        container.classList.add('carousel')
+        container.classList.add('am-active')
+        _handleStyles(container)
+    }
+
     function _handleStyles(container) {
-        for (let i = 0; i < container.children.length; i++) {
-            const element = container.children[i]
-            element.removeAttribute('class')
-            element.classList.add('spinner-element')
-            if (i === 3) {
-                element.classList.add('active')
-            } else if (i < 3 || i >= 4 && i < 7) {
-                element.classList.add('toggle')
-            } else {
-                element.classList.add('hidden')
+        if (container.children.length > 2) {
+            for (let i = 0; i < container.children.length; i++) {
+                const element = container.children[i]
+                element.removeAttribute('class')
+                element.classList.add('spinner-element')
+                if (i === 3) {
+                    element.classList.add('active')
+                } else if (i < 3 || i >= 4 && i < 7) {
+                    element.classList.add('toggle')
+                } else {
+                    element.classList.add('hidden')
+                }
+            }
+        } else if (container.children.length <= 2) {
+            let amElement = container.children[0]
+            let pmElement = container.children[1]
+            amElement.removeAttribute('class')
+            pmElement.removeAttribute('class')
+            amElement.classList.add('spinner-element')
+            pmElement.classList.add('spinner-element')
+            if (container.classList.contains('am-active')) {
+                amElement.classList.add('active')
+                pmElement.classList.add('inactive')
+            } else if (container.classList.contains('pm-active')) {
+                pmElement.classList.add('active')
+                amElement.classList.add('inactive')
+            }
+        }
+    }
+
+    function _setHiddenInputValue() {
+        _hiddenInput.setAttribute('value', _answer.timeValue)
+    }
+
+    function _handleAnswer() {
+        let hourElements = _hoursContainer.children
+        let minuteElements = _minutesContainer.children
+        let ampmElements = _ampmContainer.children
+        let hourAnswer = _findActive(hourElements)
+        let minuteAnswer = _findActive(minuteElements)
+        let ampmAnswer = _findActive(ampmElements)
+        hourAnswer = parseInt(hourAnswer)
+        minuteAnswer = parseInt(minuteAnswer)
+        _formatAnswerObj(hourAnswer, minuteAnswer, ampmAnswer)
+        _setHiddenInputValue()
+        _updateQuestionAnswerDisplay()
+    }
+
+    function _findActive(elements) {
+        for (let i = 0; i < elements.length; i++) {
+            if (elements[i].classList.contains('active')) {
+                return elements[i].innerText
             }
         }
     }
@@ -278,13 +259,30 @@ const TimeSelector = (function() {
                 }
                 return _minutesContainer
             case 'ampm':
-                break
+                _ampmContainer = container
+                const amElement = document.createElement('div')
+                const pmElement = document.createElement('div')
+                amElement.classList.add('spinner-element')
+                pmElement.classList.add('spinner-element')
+                amElement.innerText = 'AM'
+                pmElement.innerText = 'PM'
+                if (_currentTimeObj.ampm == 'AM') {
+                    _ampmContainer.classList.toggle('am-active')
+                    amElement.classList.toggle('active')
+                    pmElement.classList.toggle('inactive')
+                } else {
+                    _ampmContainer.classList.toggle('pm-active')
+                    pmElement.classList.toggle('active')
+                    amElement.classList.toggle('inactive')
+                }
+                _ampmContainer.appendChild(amElement)
+                _ampmContainer.appendChild(pmElement)
+                return _ampmContainer
         }
     }
 
     function _createClockSpinner() {
-        // let spinnerTypes = ['hours', 'minutes', 'ampm']
-        let spinnerTypes = ['hours', 'minutes']
+        let spinnerTypes = ['hours', 'minutes', 'ampm']
         const container = document.createElement('div')
         container.classList.add('spinner-container')
         container.setAttribute('id', 'clockContainer')
@@ -294,10 +292,19 @@ const TimeSelector = (function() {
 
             spinnerTimeContainer.addEventListener('wheel', (e) => {
                 if (e.wheelDelta < 0) {
-                    _handleScrollDown(spinnerTimeContainer)
+                    if (type !== 'ampm') {
+                        _handleScrollDown(spinnerTimeContainer)
+                    } else {
+                        _handleAmPmScrollDown(spinnerTimeContainer)
+                    }
                 } else {
-                    _handleScrollUp(spinnerTimeContainer)
+                    if (type !== 'ampm') {
+                        _handleScrollUp(spinnerTimeContainer)
+                    } else {
+                        _handleAmPmScrollUp(spinnerTimeContainer)
+                    }
                 }
+                _handleAnswer()
             })
 
             _handleStyles(spinnerTimeContainer)
